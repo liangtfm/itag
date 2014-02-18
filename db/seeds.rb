a423c4196d19636642902850bfea6ab68161f31f
Anthony Liang <liangtfm@gmail.com>
2014-02-18 13:52:05 -0500

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+State.create([{name: 'NY'}, {name: 'CA'}])
+
+City.create([{name: 'New York', state_id: 1}, {name: 'San Francisco', state_id: 2}])
+
+Category.create([{name: 'Italian'}, {name: 'French'}, {name: 'American'}, {name: 'Asian Fusion'}, {name: 'Chinese'}, {name: 'Japanese'}, {name: 'Korean'}, {name: 'Bars'}, {name: 'Coffee'}, {name: 'Indian'}, {name: 'Thai'}, {name: 'Seafood'}])
+
+User.create({username: 'ant', password: '123123', email: 'liangtfm@gmail.com', admin: true})
