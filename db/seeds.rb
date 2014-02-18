be153965fb2c20335f9a29734d11effd4dd0fff6
Anthony Liang <liangtfm@gmail.com>
2014-02-18 18:33:11 -0500

    Fix seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,10 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-State.create([{name: 'NY'}, {name: 'CA'}])
+State.create([{name: 'NY'}, {name: 'CA'}, {name: 'MA'}, {name: 'NH'}])
 
-City.create([{name: 'New York', state_id: 1}, {name: 'San Francisco', state_id: 2}])
+City.create([{name: 'New York', state_id: 1}, {name: 'San Francisco', state_id: 2}, {name: 'Boston', state_id: 3}, {name: 'Manchester', state_id: 4}])
 
 Category.create([{name: 'Italian'}, {name: 'French'}, {name: 'American'}, {name: 'Asian Fusion'}, {name: 'Chinese'}, {name: 'Japanese'}, {name: 'Korean'}, {name: 'Bars'}, {name: 'Coffee'}, {name: 'Indian'}, {name: 'Thai'}, {name: 'Seafood'}, {name: 'Dessert'}])
 
-User.create({username: 'ant', password: '123123', email: 'liangtfm@gmail.com', admin: true})
+User.create({username: 'ant', password: '123123', email: 'liangtfm@gmail.com', biography: "Hello!", age: 26, gender: "M", location: "NYC", admin: true})
