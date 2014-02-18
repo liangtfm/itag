6b6c51dc378f3b59a6ef69364277f13e851ef222
Anthony Liang <liangtfm@gmail.com>
2014-02-18 14:00:39 -0500

    Slight fix to restaurant show page

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,6 +10,6 @@ State.create([{name: 'NY'}, {name: 'CA'}])
 
 City.create([{name: 'New York', state_id: 1}, {name: 'San Francisco', state_id: 2}])
 
-Category.create([{name: 'Italian'}, {name: 'French'}, {name: 'American'}, {name: 'Asian Fusion'}, {name: 'Chinese'}, {name: 'Japanese'}, {name: 'Korean'}, {name: 'Bars'}, {name: 'Coffee'}, {name: 'Indian'}, {name: 'Thai'}, {name: 'Seafood'}])
+Category.create([{name: 'Italian'}, {name: 'French'}, {name: 'American'}, {name: 'Asian Fusion'}, {name: 'Chinese'}, {name: 'Japanese'}, {name: 'Korean'}, {name: 'Bars'}, {name: 'Coffee'}, {name: 'Indian'}, {name: 'Thai'}, {name: 'Seafood'}, {name: 'Dessert'}])
 
 User.create({username: 'ant', password: '123123', email: 'liangtfm@gmail.com', admin: true})
